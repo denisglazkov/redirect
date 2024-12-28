2033005a195bd0ed19635cfec6fdf9da2cb1e124
Denys Hlazkov <denisglazkov@ukr.net>
2024-12-29 01:30:56 +0200

    commented / url

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from config import AMPLITUDE_API_KEY, TELEGRAM_CHANNEL_URL, YOUTUBE_CHANNEL_URL
 app = Flask(__name__)
 
 
-
-@app.route('/', methods=['GET'])
-def hello():
-    return "Hello, World!"
+# @app.route('/', methods=['GET'])
+# def hello():
+#     return "Hello, World!"
 
 @app.route('/telegram', methods=['GET'])
 def track_click_telegram():
@@ -25,19 +24,5 @@ def track_click_youtube():
     track_click(params=params, event_name="youtube")
     return redirect(YOUTUBE_CHANNEL_URL, code=302)
 
-#for local test
 if __name__ == '__main__':
-    """
-    Entry point for running the Flask application locally.
-
-    This block of code performs the following steps:
-    1. Logs a warning indicating that the server is for development only.
-    2. Runs the Flask application on the specified host and port.
-
-    Note:
-        For production, it is recommended to use Gunicorn.
-    """
-    port = 8080
-
-    print(f"Running on port {port}")
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
