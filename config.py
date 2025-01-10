717fd1ef119dbf718cdcca98276ab76ad02fa7d9
Bohdan Bembenok <yi8@qad.com>
2025-01-10 14:23:04 +0200

    cleanup of config

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ TELEGRAM_CHANNEL_URL = os.getenv("TELEGRAM_CHANNEL_URL")
 YOUTUBE_CHANNEL_URL = os.getenv("YOUTUBE_CHANNEL_URL")
 
 if not AMPLITUDE_API_KEY or not TELEGRAM_CHANNEL_URL or not YOUTUBE_CHANNEL_URL:
-    raise EnvironmentError("Missing required environment variables: AMPLITUDE_API_KEY or TELEGRAM_CHANNEL_URL or something else")
+    raise EnvironmentError("Missing required environment variables: AMPLITUDE_API_KEY or TELEGRAM_CHANNEL_URL")
