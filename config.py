80f432b6c45ac3735c94e955fa9da7481591502e
Bohdan Bembenok <yi8@qad.com>
2025-01-10 14:21:18 +0200

    connected new relic logs

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 AMPLITUDE_API_KEY = os.getenv("AMPLITUDE_API_KEY")
 TELEGRAM_CHANNEL_URL = os.getenv("TELEGRAM_CHANNEL_URL")
 YOUTUBE_CHANNEL_URL = os.getenv("YOUTUBE_CHANNEL_URL")
+NEW_RELIC_LICENSE_KEY= os.getenv("NEW_RELIC_LICENSE_KEY")
 
-if not AMPLITUDE_API_KEY or not TELEGRAM_CHANNEL_URL or not YOUTUBE_CHANNEL_URL:
-    raise EnvironmentError("Missing required environment variables: AMPLITUDE_API_KEY or TELEGRAM_CHANNEL_URL")
+if not AMPLITUDE_API_KEY or not TELEGRAM_CHANNEL_URL or not YOUTUBE_CHANNEL_URL or not NEW_RELIC_LICENSE_KEY:
+    raise EnvironmentError("Missing required environment variables: AMPLITUDE_API_KEY or TELEGRAM_CHANNEL_URL or something else")
--- logging_config.py
+++ logging_config.py
@@ -1,7 +1,17 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
+def setup_logger():
+    """
+    Configures the logger for the Flask application.
+    """
+    # Create a logger instance
+    logger = logging.getLogger("flask-app")
+    logger.setLevel(logging.INFO)
+
+    # StreamHandler for local console logs
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(console_handler)
+
+    # New Relic will automatically instrument the logger when configured
+    return logger
