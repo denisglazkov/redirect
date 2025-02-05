652d07a274800710704353997f2bd945080d6855
Bohdan Bembenok <yi8@qad.com>
2025-02-05 20:06:29 +0200

    ip address shown

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ logger.addHandler(console_handler)
 
 @app.route('/', methods=['GET'])
 def hello():
+    user_ip = request.remote_addr or "Unknown IP"
+    
     html_content = """
     <!DOCTYPE html>
     <html lang="en">
@@ -39,6 +41,7 @@ def hello():
     <body>
         <h1>Hello, World!</h1>
         <p>Welcome to our tracking server.</p>
+        <p>Your IP address: <strong>{user_ip}</strong></p>
     </body>
     </html>
     """
